46c1aaf4f619d3d9bc6004c0c77dad6ab387309c
Nibin Mathew <mathew.nibin89@gmail.com>
2021-06-30 22:18:51 +0000

    add data listner

--- client.js
+++ client.js
@@ -1,21 +1,34 @@
+const { Server } = require("http");
 const net = require("net");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host:'135.23.223.133',  // IP address here,
-    port:50542 // PORT number here,
+    host: '135.23.223.133',  // IP address here,
+    port: 50542 // PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-conn.on("connect", () => {
-  console.log("Successfully connected to game server");
-  conn.write("Name: NIB")
-});
+  //snake initials
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: NIB");
+  });
+
+  //Move Commands
+  // conn.on("connect", () => {
+  //   setTimeout(() => {
+  //     conn.write("Move: up");
+  //   }, 100)
+  // });
+
+  conn.on("data", (data) => {
+    console.log("Server says",data);
+  });
   return conn;
 };
 
 console.log("Connecting ...");
 //connect();
-module.exports = {connect};
+module.exports = { connect };
