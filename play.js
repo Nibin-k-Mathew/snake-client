c8f847bdd86983e5336e7f5417f57a33f574532f
Nibin Mathew <mathew.nibin89@gmail.com>
2021-06-30 21:03:09 +0000

    Connection module

--- play.js
+++ play.js
@@ -1,17 +1,2 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:'localhost',  // IP address here,
-    port:50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
+const {connect} = require("./client");
 connect();
